3aef2f3865ade6c8de13e0420ae9b61bbd8903d6
edward <edward1325811540@gmail.com>
2026-05-19 16:48:26 +0800

    Initial Commit

---
+++ RehabSync/RehabSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  RehabSyncApp.swift
+//  RehabSync
+//
+//  Created by 1617-82n2 on 2026/5/19.
+//
+
+import SwiftUI
+
+@main
+struct RehabSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
