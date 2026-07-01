0d4c5415cebfd07a79d41b53a4bdedcec7ea437a
edward <edward1325811540@gmail.com>
2026-07-01 22:48:09 +0800

    left panel fix

--- RehabSync/Views/Test1.swift
+++ RehabSync/Views/Test1.swift
@@ -83,7 +83,7 @@ private struct Test1MenuTile: View {
                 .font(.system(size: 22, weight: .semibold))
         }
         .foregroundStyle(.black)
-        .padding(.horizontal, 44)
+        .padding(.horizontal, 80)
         .padding(.vertical, 14)
         .background(mint)
         .clipShape(RoundedRectangle(cornerRadius: 12))
