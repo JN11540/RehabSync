38b3cd71c7960638227c910925a7cea09dad7cf4
edward <edward1325811540@gmail.com>
2026-07-02 22:00:38 +0800

    get training menu

--- RehabSync/Views/Test1.swift
+++ RehabSync/Views/Test1.swift
@@ -163,7 +163,7 @@ private struct Test1PreviewFrame: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 20)
-                .fill(navy.opacity(0.8))
+                .fill(navy.opacity(0.95))
 
             if showTrainingImages {
                 HStack(spacing: 16) {
