ddae47601f64cf1f4346f1b7d8e1c2ee918c4224
edward <edward1325811540@gmail.com>
2026-07-03 15:57:51 +0800

    add new device

--- RehabSync/Views/Test1.swift
+++ RehabSync/Views/Test1.swift
@@ -452,7 +452,10 @@ private struct AddDeviceModal: View {
                             .fill(darkGreen)
                         RoundedRectangle(cornerRadius: 4)
                             .fill(Color(white: 0.85))
-                            .frame(width: 8, height: 120)
+                            .frame(width: 15, height: 120)
+                        RoundedRectangle(cornerRadius: 4)
+                            .fill(Color.white)
+                            .frame(width: 13, height: 120)
                     }
                     .frame(width: 20, height: 356)
 
