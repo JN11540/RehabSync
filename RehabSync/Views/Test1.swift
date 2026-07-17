aece3cac6596005fecb5f126b135a38b9088b41f
edward <edward1325811540@gmail.com>
2026-07-17 18:36:51 +0800

    add dashboard

--- RehabSync/Views/Test1.swift
+++ RehabSync/Views/Test1.swift
@@ -34,7 +34,9 @@ struct Home: View {
             }
         }
         .safeAreaInset(edge: .top) {
-            FloatingTabBar(selectedTab: $selectedTab)
+            if selectedTab != .dashboard {
+                FloatingTabBar(selectedTab: $selectedTab)
+            }
         }
         .environment(btVM)
         .overlay {
