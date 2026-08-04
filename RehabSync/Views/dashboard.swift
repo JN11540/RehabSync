bde1fe6034b2d2a7541cad12d1aeef32451ac270
1617-mac1 <edward1325811540@gmail.com>
2026-08-04 23:31:48 +0800

    hide test page

--- RehabSync/Views/dashboard.swift
+++ RehabSync/Views/dashboard.swift
@@ -222,7 +222,6 @@ private struct DashboardSidebar: View {
 
             DashboardSidebarSectionLabel(text: "一般")
             DashboardSidebarItem(item: .overview, selectedNav: $selectedNav)
-            DashboardSidebarItem(item: .test, selectedNav: $selectedNav, action: onNavigateToTest)
 
             Spacer()
 
